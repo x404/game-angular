a0f21bb8fe07818912dd1a0f435e0598c25c57c1
Aleksej <kronos2003@gmail.com>
2021-05-21 12:13:19 +0300

    generate grid (ngfor)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'game-angular';
+  objOfCells:any = {};
+  // clickCellFlag:boolean = false;
+  // start:boolean = false;
+  // errorDelayValue:boolean = false;
+  // fragment:any = document.createDocumentFragment();
+  // timer:any;
+  // prevId:any;
+
+  cellcounts: number = 100;
+  finishcount: number = 10;
+  // timeoutEl: any = document.querySelector(".i-delay");
+  // gameDiv: any = document.querySelector(".list");
+  // userCountEl: any = document.querySelector("#user_count");
+  // computerCountEl:any = document.querySelector("#computer_count");
+  // countSuccess:number = 0;
+  // countError:number = 0;
+
+  constructor() {
+    this.init()
+  }
+
+
+  init() {
+    if (this.cellcounts > 0) {
+      for (let i = 1; i <= this.cellcounts; i++) {
+       this.createObj(i);  
+       // this.renderGrid(i);
+      }
+    // this.gameDiv.appendChild(this.#fragment);
+    // this.eventsListeners();
+    }
+  }
+
+  createObj(id: number) {
+    let obj:any = {};
+    obj.id = id;
+    obj.success = false;
+    obj.error = false;
+
+    this.objOfCells[id] = {obj};
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ScoreComponent } from './score/score.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule
