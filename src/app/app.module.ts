522b12412b6f8b50f2acebab85cf5070b7bbbc91
Aleksej <kronos2003@gmail.com>
2021-05-24 16:51:33 +0300

    generate modal component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ScoreComponent } from './score/score.component';
 import { ErrorValuesPipe } from './pipes/error-values.pipe'
-import { SuccessValuesPipe } from './pipes/success-values.pipe'
+import { SuccessValuesPipe } from './pipes/success-values.pipe';
+import { ModalComponent } from './modal/modal.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ScoreComponent,
     ErrorValuesPipe,
-    SuccessValuesPipe
+    SuccessValuesPipe,
+    ModalComponent
   ],
   imports: [
     BrowserModule
